02838b46213610c92ff5aa627ad4d6293ff5c565
Grigory <76908326+Guaho121601@users.noreply.github.com>
2022-01-27 15:27:16 +0300

    New ver
    Ad game mechanic

---
+++ Programming Theory Project/Assets/Scripts/Beer.cs
@@ -0,0 +1,14 @@
+﻿public class Beer : Cup
+{
+    private int drinkVolume = 15;
+    protected override void DrinkMethod()
+    {
+        base.DrinkMethod();
+        if (GameController.healthCount > 0 && GameController.healthCount < drinkVolume)
+            GameController.healthCount -= GameController.healthCount;
+        else if (GameController.healthCount >= drinkVolume)
+            GameController.healthCount = GameController.healthCount - drinkVolume;
+        else return;
+    }
+}
+ 
---
+++ Programming Theory Project/Assets/Scripts/Tee.cs
@@ -0,0 +1,12 @@
+﻿public class Tee : Cup
+{
+    private int drinkVolume = 5;
+    protected override void DrinkMethod()
+    {
+        base.DrinkMethod();
+        if (GameController.healthCount <= 100 - drinkVolume)
+            GameController.healthCount = GameController.healthCount + drinkVolume;
+        else
+            GameController.healthCount = GameController.healthCount + (100 - GameController.healthCount);
+    }
+}
